203ac95627b03e3b2b7c7570eae3c4bf869b616b
David Ulicny <ulicny.david@gmail.com>
2022-08-07 05:52:37 +0200

    config

--- dd-app/__init__.py
+++ dd-app/__init__.py
@@ -5,9 +5,10 @@ from flask import (Flask, render_template)
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    print(app.config)
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.py', silent=False)
     else:
         app.config.from_mapping(test_config)
 
